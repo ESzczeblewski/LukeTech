036fb1fa1a2ae185eab4c9a8b3a3bc63a4a4a056
Kilu691 <kilu691@gmail.com>
2021-03-17 22:17:50 +0100

    Mobile nav 1.0

--- dist/main.c48f6146.js
+++ dist/main.c48f6146.js
@@ -118,7 +118,20 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 
   return newRequire;
 })({"src/js/main.js":[function(require,module,exports) {
-console.log('elo');
+var nav = document.querySelector('.nav');
+var topOfNav = nav.offsetTop;
+var navOffset = nav.offsetHeight;
+console.log(topOfNav);
+
+function fixNav() {
+  if (window.scrollY >= navOffset) {
+    nav.classList.add('nav--sticky');
+  } else {
+    nav.classList.remove('nav--sticky');
+  }
+}
+
+window.addEventListener('scroll', fixNav);
 },{}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -147,7 +160,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61323" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53450" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,17 @@
-console.log('elo');
+const nav = document.querySelector('.nav');
+const topOfNav = nav.offsetTop;
+const navOffset = nav.offsetHeight;
+
+console.log(topOfNav);
+
+function fixNav() {
+
+    if (window.scrollY >= navOffset) {
+        nav.classList.add('nav--sticky');
+    } else {
+        nav.classList.remove('nav--sticky');
+    }
+
+}
+
+window.addEventListener('scroll', fixNav);
